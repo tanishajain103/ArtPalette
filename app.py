d2f569d06d145c9b36dec0ee63a524e8c79f9fd8
Tanisha <tanishajain1974@gmail.com>
2026-09-21 10:32:26 +0530

    Fix Flask server binding for deployment

--- app.py
+++ app.py
@@ -182,9 +182,11 @@ def send_example(filename):
     return send_from_directory('examples', filename)
 
 
-if __name__ == '__main__': 
-    from werkzeug.serving import run_simple 
-    run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True)
-
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=True
+    )
 
 
